e126642e87554f5ed742ccd5710f53ada5157f91
Michele Castellana <michele.castellana@cern.ch>
2016-02-06 15:36:59 +0100

    Use an argument parser in the main

--- main.cc
+++ main.cc
@@ -4,11 +4,19 @@
 int main(int argc, char*argv[])
 {
     /*
-     register simulators (i.e. ghdl & systemverilog)
 
-     for each input file {
-        create a simulator instance that works with associated file
+     create argument parser instance.
+     forward input to the argument parser.
+     
+     create manager instance.
+
+     // for verilog files
+     if( *argument parser has verilog files as input* ) {
+        create verilog sim.
+        pass files to verilog sim.
+        add instance to the manager.
      }
+     // for VHDL files, the dual of the above pseudocode.
 
      manager.run();
      */
