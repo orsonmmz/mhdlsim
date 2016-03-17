09e648f726ca2ac82491f095a6130ee638449fd5
Michele Castellana <michele.castellana@cern.ch>
2016-03-17 10:51:23 +0100

    Fix a typedef and other small changes

--- simulator.h
+++ simulator.h
@@ -1,37 +1,35 @@
-/*
- * Copyright (c) 2016 CERN
- * @author Maciej Suminski <maciej.suminski@cern.ch>
- *
- * This source code is free software; you can redistribute it
- * and/or modify it in source code form under the terms of the GNU
- * General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
- */
-
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
 
+/*
+ * Copyright (c) 2016 CERN
+ * @author Michele Castellana <michele.castellana@cern.ch>
+ *
+ * This source code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ///> Universal simulation time unit.
-typedef sim_time_t unsigned long long;
+typedef unsigned long long sim_time_t;
 
 class Simulator {
 public:
-    Simulator();
-    virtual ~Simulator();
+    Simulator() {};
+    virtual ~Simulator() {};
 
     /**
      * @brief Initializes the simulator.
-     * @return 0 in case of success.
+     * @return 0 if success. Non zero value in case of failure.
      */
     virtual int initialize() = 0;
 
@@ -39,11 +37,11 @@ public:
      * @brief Notifies the simulator that a net value has changed.
      * @param net is the net which value has just changed.
      */
-    void notify(Net* net);
+    virtual void notify(Net* net) = 0;
 
     /**
      * @brief Executes the next event from the event queue.
-     * @return 0 in case of success.
+     * @return 0 if success. Non zero value in case of failure.
      */
     virtual int step_event() = 0;
 
@@ -51,12 +49,12 @@ public:
      * @brief Returns the timestamp of the next event in the event queue.
      * @return Timestamp of the next event to be executed.
      */
-    virtual sim_time_t next_event() const;
+    virtual sim_time_t next_event() const = 0;
 
     /**
      * @brief Returns the current simulation time.
      */
-    virtual sim_time_t current_time() const;
+    virtual sim_time_t current_time() const = 0;
 
     /**
      * @brief Advances the simulation by time. All events in the queue will be
