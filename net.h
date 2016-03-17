9077da15688f11d04897a93eb3161fa09e7fa8b8
Michele Castellana <michele.castellana@cern.ch>
2016-03-17 10:51:32 +0100

    Minor changes in the net interface

--- net.h
+++ net.h
@@ -1,21 +1,20 @@
 /*
  * Copyright (c) 2016 CERN
  * @author Maciej Suminski <maciej.suminski@cern.ch>
+ * @author Michele Castellana <michele.castellana@cern.ch>
  *
- * This source code is free software; you can redistribute it
- * and/or modify it in source code form under the terms of the GNU
- * General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * This source code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This source code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef NET_H
@@ -29,20 +28,22 @@ public:
     Net(const std::string& name, int width = 1);
     virtual ~Net();
 
-    // TODO should it be possible only for the Manager?
-    virtual void set_value(const value_vec_t& v) { assert(v.size() == width()); val_ = v; }
-    virtual const value_vec_t& value() const { return val_; }
-    int width() { return val_.size(); }
-
     ///> Possible net values
     enum value_t { V0, V1, VX, VZ, VH, VL };
     typedef std::vector<value_t> value_vec_t;
+
+    // TODO should it be possible only for the Manager?
+    virtual void set_value(const value_vec_t& v) { assert(v.size() == width()); val_ = v; }
+    const std::string& name() const { return name_; }
+    virtual const value_vec_t& value() const { return val_; }
+    int width() { return val_.size(); }
 
     // TODO do we need other net types? in fact everything in the end is
     // just a bunch of bits, so probably could be represented using this class as well
 
 protected:
     value_vec_t val_;
+    const std::string name_;
 };
 
 /**
