fb2c1df267628e721d9f540d6255b549fafff183
Michele Castellana <michele.castellana@cern.ch>
2016-03-17 10:48:49 +0100

    Improve and simplify elaborator interface

--- elaborator.h
+++ elaborator.h
@@ -24,40 +24,74 @@
 
 class Elaborator : public virtual FileParamHandler {
 public:
-    Elaborator();
-    virtual ~Elaborator();
+    Elaborator() :
+       spec_to_find_(nullptr),
+       instance_found_(nullptr) {};
+    virtual ~Elaborator() {};
 
-    /**
-     * @brief Performs elaboration of the required entity/arch.
-     * @param ent entity name.
-     * @param arch architecture name.
-     * @return unknown module/architecture discovered during elaboration.
-     */
-    virtual ModuleSpec& elaborate(const std::string& ent, const std::string& arch) = 0;
+    enum status { NOT_FOUND, FOUND, NEED_ANOTHER };
 
     /**
      * @brief Complete the elaboration.
      * @param module module/architecture needed to continue the elaboration.
      * @return unknown module/architecture discovered during elaboration.
+     *         NULL in any other case.
      */
-    virtual ModuleSpec& elaborate(ModuleInstances& module) = 0;
+    virtual ModuleSpec* elaborate(ModuleInstance* module = nullptr) = 0;
+
+   /**
+     * @brief To be used after the call of the method "instantiate".
+     * @return A new instance if the compiler status after the "instantiate"
+     *         method is FOUND. Nullptr otherwise.
+     * @warning only the first call will actually return the spec.
+     *          Subsequent call will return nullptr
+     */
+    ModuleInstance* get_instance() {
+       ModuleInstance* tmp = instance_found_;
+       if( instance_found_ ) {
+          instance_found_ = nullptr;
+       }
+       return tmp;
+    };
 
     /**
-     * @brief Creates an instance with a given name.
-     * This function is to be called by the Manager. It will also assign nets
-     * to ports.
+     * @brief To be used after the call of the method "instantiate".
+     * @return The new required spec if the compiler status after the
+     *         "instantiate" method is NEED_ANOTHER. Nullptr otherwise.
+     * @warning only the first call will actually return the spec.
+     *          Subsequent call will return nullptr
      */
-    ModuleInstance& instantiate(const ModuleSpec& iface);
+    ModuleSpec* get_spec() {
+       ModuleSpec* tmp = spec_to_find_;
+       if( spec_to_find_ ) {
+          spec_to_find_ = nullptr;
+       }
+       return tmp;
+    };
+
+    /**
+     * @brief Answer the question: Is possible to continue with the emit_code?
+     * @return True if there are no internal problems. False otherwise.
+     */
+    virtual bool can_continue() = 0;
+
+    /**
+     * @brief Emit code for the simulation.
+     * If something went wrong in the elaboration phase, do nothing.
+     * @return 0 if success. Non zero value in case of failure.
+     */
+    virtual int emit_code() = 0;
+
+    /**
+     * @brief Given a spec returns the compiler status after the request.
+     * @param result the status of the compiler.
+     * @return the status of the compiler.
+     */
+    virtual status instantiate(ModuleSpec& iface) = 0;
 
 protected:
-    ///> Modules provided by this simulator instance. They can be instantiated
-    ///> as required. The string key is the name of the module, as defined
-    ///> in its interface.
-    std::map<const std::string, ModuleInterface>;
-
-    ///> Instances of modules handled by this simulator instance. The string key
-    ///> is the name of an instance, not the name of the module.
-    std::map<const std::string, ModuleInstance>;
+    ModuleSpec* spec_to_find_;
+    ModuleInstance* instance_found_;
 
 };
 
